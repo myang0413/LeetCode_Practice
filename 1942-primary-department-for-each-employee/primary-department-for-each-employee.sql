860b058c8b4ffab319d29a02f271413a453ad223
Mungyu Yang <96772690+myang0413@users.noreply.github.com>
2024-02-19 08:22:44 +0900

    Time: 575 ms (39.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,19 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    employee_id,
+    department_id
+FROM
+    Employee
+WHERE
+    primary_flag = 'Y'
+UNION ALL
+SELECT
+    employee_id,
+    MIN(department_id) as department_id 
+FROM
+    Employee
+GROUP BY
+    employee_id
+HAVING
+    COUNT(*) = 1
+    AND MAX(primary_flag) = 'N'
